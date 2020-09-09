11d66027af7741d7014faae37d9ff0851b25e403
JulienS69 <julien.seux69@gmail.com>
2020-09-09 16:24:11 +0200

    v1

---
+++ GSBC#V1/PPEGSB/DAOMedicament.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Metier;
+using Persistance;
+
+namespace DAO
+{
+	public class DAOMedicament
+	{
+		///<summary>
+		///Fournit la liste de tous les Clients.
+		///</summary>
+		///<returns>
+		///Une collection (list) de Clients
+		///</returns>
+		public static List<Medicament> GetClients()
+		{
+			List <Medicament> medicaments  = null;
+			string selectMedicaments = "SELECT MED_DEPOTLEGAL, MED_NOMCOMMERCIAL, MED_COMPOSITION, MED_EFFETS, MED_CONTREINDIC, MED_PRIXECHANTILLON FROM MEDICAMENT";
+			DataTable dataTable = DBInterface.Select(selectMedicaments);
+			//Le Datatable retourné est transformé en collection de Client
+			if (dataTable != null)
+			{
+				medicaments = new List<Medicament>();
+				foreach (DataRow row in dataTable.Rows)
+				{
+					Medicament client = CreerClient(row); ;
+					medicaments.Add(client);
+				}
+			}
+			return medicaments;
+		}
+
+		//Construction d'une instance de Client à partir d'une ligne du résultat de la requête.
+		//Rappel : on souhaite que la couche "Présentation" ne soit pas liée à la base de données.
+		//Elle ne doit travailler qu'avec des objets métiers.
+		private static Medicament CreerClient(DataRow row)
+		{
+			Medicament medicament = new Medicament();
+			medicament.DepotLegal = row["MED_DEPOTLEGAL"].ToString();
+			medicament.NomCommercial = row["MED_NOMCOMMERCIAL"].ToString();
+			medicament.Composition = row["MED_COMPOSITION"].ToString();
+			medicament.Effets = row["MED_EFFETS"].ToString();
+			medicament.ContreIndications = row["MED_CONTREINDIC"].ToString();
+			if (row["MED_PRIXECHANTILLON"].ToString() != "")
+			{
+				medicament.PrixEchantillon = Convert.ToDouble(row["MED_PRIXECHANTILLON"].ToString());
+			}
+			else
+			{
+				medicament.PrixEchantillon = 0;
+			}
+			return medicament;
+		}
+	}
+}
---
+++ GSBC#V1/PPEGSB/FMenuPrincipal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPEGSB
+{
+    public partial class FMenuPrincipal : Form
+    {
+        public FMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void médicamentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Medicaments medicaments = new Medicaments();
+            medicaments.ShowDialog();
+        }
+
+        private void quitterLapplicationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RAPPORT_VISITE Rapport_visite = new RAPPORT_VISITE();
+            Rapport_visite.ShowDialog();
+        }
+
+        private void aProposToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            APropos apropos = new APropos();
+            apropos.ShowDialog();
+        }
+    }
+}
